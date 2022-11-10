8a19479e78a445bb792d30aa477a51adcddd8c41
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2022-11-10 22:08:35 +0700

    chore(fetch): apply prettier

--- fetch.test.ts
+++ fetch.test.ts
@@ -1,7 +1,7 @@
-import { sum } from './fetch';
-import test from 'node:test';
-import { strict as assert } from 'node:assert';
+import { sum } from "./fetch";
+import test from "node:test";
+import { strict as assert } from "node:assert";
 
-test('sum', (t) => {
-    assert.deepEqual(sum(2,3),5);
+test("sum", (t) => {
+  assert.deepEqual(sum(2, 3), 5);
 });
