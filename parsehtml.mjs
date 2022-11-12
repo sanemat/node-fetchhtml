f0906d73493e20f1d42fe38102510fac5a22bb55
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2022-11-12 20:54:39 +0700

    feat(parsehtml): count links and images

--- parsehtml.mjs
+++ parsehtml.mjs
@@ -1,10 +1,17 @@
+import { load } from "cheerio";
+
 /**
  * @param {string} html - html
  * @returns {{ imageUrls: string[], linkUrls: string[] }} - included urls
  */
 export async function parsehtml(html) {
+  let $ = load(html);
+  let imageUrls = [];
+  let linkUrls = [];
+  $("img").map(() => imageUrls.push(""));
+  $("a").map(() => linkUrls.push(""));
   return {
-    imageUrls: [],
-    linkUrls: [],
+    imageUrls: imageUrls,
+    linkUrls: linkUrls,
   };
 }
