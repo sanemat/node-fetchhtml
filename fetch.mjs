b4b6f21f26d2d94bdda34ae06dda29eca37ac702
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2022-11-12 18:32:23 +0700

    feat(fetch): add cli interface

--- fetch.mjs
+++ fetch.mjs
@@ -6,4 +6,16 @@ export function sum(a, b) {
   return a + b;
 }
 
-console.log(await downloadhtml("https://example.com"));
+const myArgs = process.argv.slice(2);
+
+if (myArgs.length > 0) {
+  if (myArgs[0] === "--metadata" && myArgs.length === 2) {
+    console.log(`show metadata ${myArgs[1]}`);
+  } else {
+    try {
+      console.log(await downloadhtml("https://example.com"));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
