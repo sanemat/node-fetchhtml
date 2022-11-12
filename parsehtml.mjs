8fee587b53b8351c8623d5c6804ce4be03f09f04
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2022-11-12 17:48:21 +0700

    docs(parsehtml): add signature

--- parsehtml.mjs
+++ parsehtml.mjs
@@ -1,3 +1,7 @@
+/**
+ * @param {string} html - html
+ * @returns {{ imageUrls: string[], linkUrls: string[] }} - included urls
+ */
 export async function parsehtml(html) {
     return {
         imageUrls: [],
