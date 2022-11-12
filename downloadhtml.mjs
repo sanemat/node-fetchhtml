8f702dfcfed6c91ae48eb0374fba2857efacfd92
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2022-11-12 17:48:58 +0700

    docs(downloadhtml): add signature

--- downloadhtml.mjs
+++ downloadhtml.mjs
@@ -1,5 +1,9 @@
 import fetch from "node-fetch";
 
+/**
+ * @param {string} url - url
+ * @returns {string} - html
+ */
 export async function downloadhtml(url) {
   const response = await fetch(url);
   return await response.text();
